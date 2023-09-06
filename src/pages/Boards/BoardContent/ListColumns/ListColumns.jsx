a34a8317abf9aedf3c7dc66e0a8e9b981ba4e4df
HDTran91 <khoaiga0103@gmail.com>
2023-09-06 15:56:37 -0500

    Drag_Drop_trello_column

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,44 +2,49 @@ import Box from '@mui/material/Box'
 import Column from './Column/Column'
 import Button from '@mui/material/Button'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListColumns({ columns }) {
-  return (
-    <Box sx= {{
-      bgcolor: 'inherit',
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      overflowX: 'auto',
-      overflowY: 'hidden',
-      '&::-webkit-scrollbar-track': { m: 2 }
-    }}>
-      {/* Box Column1 */}
-      {columns?.map( column => (
-        <Column key = {column._id} column = {column} />
-      ))}
 
-      {/* Box Add new Column */}
-      <Box sx = {{
-        minWidth: '200px',
-        maxWidth: '200px',
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643': '#ebecf0'),
-        mx: 2,
-        borderRadius: '6px',
-        height: 'fit-content',
-        bgColor: 'ffffff3d'
+  /** sortableContext require items= [id1,id2,id3], not [id: 'id1', id: 'id2'] */
+  return (
+    <SortableContext items ={columns?.map(c =>c._id)} strategy ={horizontalListSortingStrategy}>
+      <Box sx= {{
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        '&::-webkit-scrollbar-track': { m: 2 }
       }}>
-        <Button
-          startIcon = {<NoteAddIcon />}
-          sx= {{
-            color: 'white',
-            width: '100%',
-            justifyContent: 'flex-start',
-            pl: 2.5,
-            py: 1
-          }}>Add new column</Button>
+        {/* Box Column1 */}
+        {columns?.map( column => (
+          <Column key = {column._id} column = {column} />
+        ))}
+
+        {/* Box Add new Column */}
+        <Box sx = {{
+          minWidth: '200px',
+          maxWidth: '200px',
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643': '#ebecf0'),
+          mx: 2,
+          borderRadius: '6px',
+          height: 'fit-content',
+          bgColor: 'ffffff3d'
+        }}>
+          <Button
+            startIcon = {<NoteAddIcon />}
+            sx= {{
+              color: 'white',
+              width: '100%',
+              justifyContent: 'flex-start',
+              pl: 2.5,
+              py: 1
+            }}>Add new column</Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
 
