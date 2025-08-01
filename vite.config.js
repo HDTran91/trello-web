cec1ca3fd9e7b08fbc94c21d84e51dba39664ff1
HDTran91 <khoaiga0103@gmail.com>
2025-07-31 22:28:11 -0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr()
-  ],
+  base: '/', // ✅ Make sure this is a string, not an object
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       { find: '~', replacement: '/src' }
     ]
   }
 })
-
